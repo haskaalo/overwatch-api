e47b574af1535a7b21bdeea38f3aa1bc803b0c3a
Haskaalo <haskaalo@gmail.com>
2018-01-30 19:07:37 -0500

    Bind to 0.0.0.0 if production

--- packages/webapi/server.js
+++ packages/webapi/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, 'localhost', () => {
-    console.log(`Server started on port ${3000} localhost`);
+app.listen(process.env.PORT || 3000, process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost', () => {
+    console.log(`Server started on port ${3000}`);
 });
